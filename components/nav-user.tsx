7f24a39c313b9d64885560746a884567e999558d
Emmanuel Hernandez <e805177@gmail.com>
2025-07-01 00:47:14 -0600

    fix(nav-user): replace Avatar component with conditional background color based on SAT flag

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -53,7 +53,7 @@ export function NavUser() {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className={`h-8 w-8 rounded-lg  ${!SAT ? 'bg-red-500' : 'bg-green-500'}`}/>
+              <div className={`h-8 w-8 rounded-lg  ${!SAT ? 'bg-red-500' : 'bg-green-500'}`} />
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user?.name}</span>
                 <span className="truncate text-xs">{user?.email}</span>
@@ -69,10 +69,7 @@ export function NavUser() {
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg  ">
-                  <AvatarImage src={user?.name} alt={user?.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
+                <div className={`h-8 w-8 rounded-lg  ${!SAT ? 'bg-red-500' : 'bg-green-500'}`} />
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">{user?.name}</span>
                   <span className="truncate text-xs">{user?.email}</span>
