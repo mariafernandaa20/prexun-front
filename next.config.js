25d01c9ff7e2f9b39700499350a1f55eff05ebb7
Emmanuel Hernandez <e805177@gmail.com>
2026-03-03 23:05:34 -0600

    Actualizar configuración de encabezados en next.config.js para la ruta de asistencia pública

--- next.config.js
+++ next.config.js
@@ -8,7 +8,23 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: '/asistencia-publica',
+        headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          { key: 'Strict-Transport-Security', value: 'max-age=63072000' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "frame-ancestors 'self' https://admin.prexun.com http://localhost:3000 https://localhost:3000 http://localhost:3001 https://localhost:3001",
+          },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+        ],
+      },
+      {
+        source: '/((?!asistencia-publica).*)',
         headers: [
           { key: 'X-DNS-Prefetch-Control', value: 'on' },
           { key: 'Strict-Transport-Security', value: 'max-age=63072000' },
